6b5a8309e4bc868cf7b697a5a05bce57cbcd36d0
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-08-01 06:08:01 -0700

    Revert "Trimmed the branch to only the ament_uncrustify sub directory"
    This reverts commit 5a4b1a1220dd6b1a098b29d6d6ff9c453d4781d9.

---
+++ ament_lint/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='ament_lint',
+    version='0.7.5',
+    packages=find_packages(exclude=['test']),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Dirk Thomas',
+    author_email='dthomas@osrfoundation.org',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    url='https://github.com/ament/ament_lint',
+    download_url='https://github.com/ament/ament_lint/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Providing common API for ament linter packages.',
+    long_description="""\
+Providing common API for ament linter packages, e.g. the `linter` marker for
+pytest.""",
+    license='Apache License, Version 2.0',
+    entry_points={
+        'pytest11': [
+            'ament_lint = ament_lint.pytest_marker',
+        ],
+    },
+)
