01b993b86dd826b2fb113ab6637cc22350bd22bf
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-10-11 16:25:02 -0400

    Rebase from 'upstream'

--- ament_pep8/setup.py
+++ ament_pep8/setup.py
@@ -5,10 +5,12 @@ package_name = 'ament_pep8'
 
 setup(
     name=package_name,
-    version='0.7.9',
+    version='0.7.11',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
     package_data={'': [
